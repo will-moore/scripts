c2a16cd69a7d81077f74907d60941a05032d54aa
Will Moore <will@lifesci.dundee.ac.uk>
2013-09-30 11:53:04 +0100

    Fix remaining PEP8 issues (whitespace)

--- omero/export_scripts/Batch_Image_Export.py
+++ omero/export_scripts/Batch_Image_Export.py
@@ -386,7 +386,7 @@ def batchImageExport(conn, scriptParams):
     # do the saving to disk
     for img in images:
         if img._prepareRE().requiresPixelsPyramid():
-            log(  "  ** Can't export a 'Big' image to %s. **" % format)
+            log("  ** Can't export a 'Big' image to %s. **" % format)
             if len(images) == 1:
                 return None, "Can't export a 'Big' image to %s." % format
             continue
@@ -397,8 +397,8 @@ def batchImageExport(conn, scriptParams):
             saveAsOmeTiff(conn, img, folder_name)
         else:
             if img._prepareRE().requiresPixelsPyramid():
-                log(  "  ** Can't export a 'Big' image to OME-TIFF. **")
-            log("\n----------- Saving planes from image: '%s' ------------" 
+                log("  ** Can't export a 'Big' image to OME-TIFF. **")
+            log("\n----------- Saving planes from image: '%s' ------------"
                 % img.getName())
             sizeC = img.getSizeC()
             sizeZ = img.getSizeZ()
